134665997f729d079a7a6b4f1becd3749a70a835
박태훈(IT서비스본부/개발실/프론트엔드팀/프로) <fethpiao@emforce.co.kr>
2023-01-22 19:42:45 +0900

    init

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAVLgprhyloxfbF1Q5yyAOntP8UZm0t-gc",
+    authDomain: "nwitter-84998.firebaseapp.com",
+    projectId: "nwitter-84998",
+    storageBucket: "nwitter-84998.appspot.com",
+    messagingSenderId: "670560093122",
+    appId: "1:670560093122:web:eb22d8b83d3a47dc344c5b"
+};
