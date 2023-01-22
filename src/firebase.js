9a312e520794885b5ab3dc8fa96fb64e077969fd
박태훈(IT서비스본부/개발실/프론트엔드팀/프로) <fethpiao@emforce.co.kr>
2023-01-22 20:16:58 +0900

    init

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,17 @@
+// import firebase from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAVLgprhyloxfbF1Q5yyAOntP8UZm0t-gc",
-    authDomain: "nwitter-84998.firebaseapp.com",
-    projectId: "nwitter-84998",
-    storageBucket: "nwitter-84998.appspot.com",
-    messagingSenderId: "670560093122",
-    appId: "1:670560093122:web:eb22d8b83d3a47dc344c5b"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
+
+export default firebase.initializeApp(firebaseConfig)
